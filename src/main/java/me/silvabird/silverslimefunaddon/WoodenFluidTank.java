d7f4d427caee904cf4274c010fa9e6b0d63df4d6
SilvaBird <jonnyawade@gmail.com>
2022-10-25 20:29:06 -0500

    nope

--- src/main/java/me/silvabird/silverslimefunaddon/WoodenFluidTank.java
+++ src/main/java/me/silvabird/silverslimefunaddon/WoodenFluidTank.java
@@ -1,4 +1,35 @@
 package me.silvabird.silverslimefunaddon;
+import io.github.thebusybiscuit.slimefun4.api.events.PlayerRightClickEvent;
+import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
+import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
+import io.github.thebusybiscuit.slimefun4.core.handlers.BlockUseHandler;
+import io.github.thebusybiscuit.slimefun4.core.handlers.ItemUseHandler;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
 
-public class WoodenFluidTank {
+public class WoodenFluidTank extends SlimefunItem {
+    public WoodenFluidTank(ItemGroup itemGroup, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
+        super(itemGroup, item, recipeType, recipe);
+    }
+
+    @Override
+    public void PreRegistor() {
+        BlockUseHandler blockUseHandler = this::onBlockRightClick;
+        addItemHandler(blockUseHandler);
+    }
+
+
+    private void onBlockRightClick(PlayerRightClickEvent e) {
+        if e.getPlayer().getItemInUse() == Material.BUCKET {
+
+        } else if e.getPlayer().getItemInUse() == Material.WATER_BUCKET{
+
+        } else if e.getPlayer().getItemInUse() == Material.HONEY_BOTTLE{
+
+        } else if e.getPlayer().getItemInUse() == Material.MILK_BUCKET{
+
+        }
+    }
 }
